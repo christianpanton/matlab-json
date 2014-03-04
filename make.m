2229a2fe6093e1a8389db07e7ab9d29f2e90fe98
Christian Panton <christian@panton.org>
2014-03-04 12:04:35 +0100

    fixing bug in tojson, freeing memory too early
    thanks Kyle

--- make.m
+++ make.m
@@ -1,3 +1,4 @@
+clear mex % avoid lock on file
 if ispc
     % Windows7/Visual Studio 2010
     mex -ljson -g LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:MSVSRT.lib /NODEFAULTLIB:LIBCMT.lib" -IC:\Users\panton\code\ -LC:\Users\panton\code\json-c\Release\ fromjson.c
