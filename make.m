e3d1d90f0292a8c276896e4980156a326917d547
James Myatt <james.myatt@tessella.com>
2015-02-05 17:12:58 +0000

    Update make file to automatically locate json library for all windows targets (release/debug, win32/64)

--- make.m
+++ make.m
@@ -1,23 +1,33 @@
 function make(varargin)
 
-mexargs = [{'-ljson-c', '-g'} varargin];
+mexargs_common = [{'-g'} varargin];
+mexargs_json = [mexargs_common {'-ljson-c'}];
 
 if ispc
     % Windows7/Visual Studio 2010
     
     % Assume json-c and matlab-json are checked out in the same directory
     json_c_path = fullfile(fileparts(pwd), 'json-c');
+    json_lib_path = json_c_path;
+    if strcmp(computer('arch'), 'win64')
+        json_lib_path = fullfile(json_lib_path, 'x64');
+    end
+    if any(strcmp(mexargs_json, '-g'))
+        json_lib_path = fullfile(json_lib_path, 'Debug');
+    else
+        json_lib_path = fullfile(json_lib_path, 'Release');
+    end
     
-    mexargs = [{
-        ['-I' json_c_path], ['-L' fullfile(json_c_path, 'Release')], ...
-        'LINKFLAGS="$LINKFLAGS' '/NODEFAULTLIB:MSVSRT' '/NODEFAULTLIB:MSVCRTD' '/NODEFAULTLIB:LIBCMT"', ...
-        } mexargs];
-    mex(mexargs{:}, 'fromjson.c')
-    mex(mexargs{:}, 'tojson.c')
+    mexargs_json = [{
+        ['-I' json_c_path], ['-L' json_lib_path], ...
+        ... % 'LINKFLAGS="$LINKFLAGS' '/NODEFAULTLIB:MSVSRT' '/NODEFAULTLIB:MSVCRTD' '/NODEFAULTLIB:LIBCMT' '/NODEFAULTLIB:LIBCMTD"', ...
+        } mexargs_json];
+    mex(mexargs_json{:}, 'fromjson.c')
+    mex(mexargs_json{:}, 'tojson.c')
 else
     % Linux/Ubuntu/GCC
-    mex(mexargs{:}, 'fromjson.c')
-    mex(mexargs{:}, '-lm', 'tojson.c')
+    mex(mexargs_json{:}, 'fromjson.c')
+    mex(mexargs_json{:}, '-lm', 'tojson.c')
 end
 
-mex setjsonfield.c
+mex(mexargs_common{:}, 'setjsonfield.c')
